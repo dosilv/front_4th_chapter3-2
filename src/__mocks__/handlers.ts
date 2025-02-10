d7dacc24049ff7b47b6eb7e9f45192e9561909db
Dana <do.silver2@gmail.com>
2025-02-10 23:07:15 +0900

    ✨ 반복 일정을 위한 handlers 추가

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,3 +1,5 @@
+import { randomUUID } from 'crypto';
+
 import { http, HttpResponse } from 'msw';
 
 import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
@@ -31,6 +33,56 @@ export const handlers = [
     const index = events.findIndex((event) => event.id === id);
 
     if (index !== -1) {
+      return new HttpResponse(null, { status: 204 });
+    }
+
+    return new HttpResponse(null, { status: 404 });
+  }),
+
+  http.post('/api/events-list', async ({ request }) => {
+    const { events: newEventsList } = (await request.json()) as { events: Event[] };
+    const repeatId = randomUUID();
+
+    const createdEvents = newEventsList.map((event: Event) => {
+      const isRepeatEvent = event.repeat.type !== 'none';
+      return {
+        ...event,
+        id: randomUUID(),
+        repeat: {
+          ...event.repeat,
+          id: isRepeatEvent ? repeatId : undefined,
+        },
+      };
+    });
+
+    return HttpResponse.json(createdEvents, { status: 201 });
+  }),
+
+  http.put('/api/events-list', async ({ request }) => {
+    const { events: updatedEventsList } = (await request.json()) as { events: Event[] };
+    let isUpdated = false;
+
+    const updatedEvents = events.map((event) => {
+      const matchingEvent = updatedEventsList.find((e: Event) => e.id === event.id);
+      if (matchingEvent) {
+        isUpdated = true;
+        return { ...event, ...matchingEvent };
+      }
+      return event;
+    });
+
+    if (isUpdated) {
+      return HttpResponse.json(updatedEvents);
+    }
+
+    return new HttpResponse(null, { status: 404 });
+  }),
+
+  http.delete('/api/events-list', async ({ request }) => {
+    const { eventIds } = (await request.json()) as { eventIds: string[] };
+    const eventExists = events.some((event) => eventIds.includes(event.id));
+
+    if (eventExists) {
       return new HttpResponse(null, { status: 204 });
     }
 

809d01e6ad29cba8955e0c523421d1ce54c1cc22
Dana <do.silver2@gmail.com>
2025-02-10 23:57:51 +0900

    ✨ 반복 일정을 위한 handlersUtils 수정

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -1,9 +1,10 @@
+import { randomUUID } from 'crypto';
+
 import { http, HttpResponse } from 'msw';
 
 import { server } from '../setupTests';
 import { Event } from '../types';
 
-// ! Hard 여기 제공 안함
 export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
   const mockEvents: Event[] = [...initEvents];
 
@@ -16,6 +17,26 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
       newEvent.id = String(mockEvents.length + 1); // 간단한 ID 생성
       mockEvents.push(newEvent);
       return HttpResponse.json(newEvent, { status: 201 });
+    }),
+    http.post('/api/events-list', async ({ request }) => {
+      const { events: newEventsList } = (await request.json()) as { events: Event[] };
+      console.log('newEventsList');
+      console.log(newEventsList);
+      const createdEvents = newEventsList.map((event: Event) => {
+        const isRepeatEvent = event.repeat.type !== 'none';
+        const repeatId = randomUUID();
+
+        return {
+          ...event,
+          id: randomUUID(),
+          repeat: {
+            ...event.repeat,
+            id: isRepeatEvent ? repeatId : undefined,
+          },
+        };
+      });
+      mockEvents.push(...createdEvents);
+      return HttpResponse.json(createdEvents, { status: 201 });
     })
   );
 };
@@ -46,6 +67,42 @@ export const setupMockHandlerUpdating = () => {
       repeat: { type: 'none', interval: 0 },
       notificationTime: 5,
     },
+    {
+      id: '3',
+      title: '격일 일정',
+      date: '2025-02-10',
+      startTime: '11:00',
+      endTime: '12:00',
+      description: '격일 일정',
+      location: '회의실',
+      category: '업무 회의',
+      repeat: { type: 'daily', interval: 2 },
+      notificationTime: 5,
+    },
+    {
+      id: '4',
+      title: '격일 일정',
+      date: '2025-02-12',
+      startTime: '11:00',
+      endTime: '12:00',
+      description: '격일 일정',
+      location: '회의실',
+      category: '업무 회의',
+      repeat: { type: 'daily', interval: 2 },
+      notificationTime: 5,
+    },
+    {
+      id: '5',
+      title: '격일 일정',
+      date: '2025-02-14',
+      startTime: '11:00',
+      endTime: '12:00',
+      description: '격일 일정',
+      location: '회의실',
+      category: '업무 회의',
+      repeat: { type: 'daily', interval: 2 },
+      notificationTime: 5,
+    },
   ];
 
   server.use(
@@ -57,7 +114,11 @@ export const setupMockHandlerUpdating = () => {
       const updatedEvent = (await request.json()) as Event;
       const index = mockEvents.findIndex((event) => event.id === id);
 
-      mockEvents[index] = { ...mockEvents[index], ...updatedEvent };
+      mockEvents[index] = {
+        ...mockEvents[index],
+        ...updatedEvent,
+        repeat: { type: 'none', interval: 0, id: undefined },
+      };
       return HttpResponse.json(mockEvents[index]);
     })
   );
@@ -76,6 +137,42 @@ export const setupMockHandlerDeletion = () => {
       category: '기타',
       repeat: { type: 'none', interval: 0 },
       notificationTime: 10,
+    },
+    {
+      id: '2',
+      title: '격일 일정',
+      date: '2025-02-10',
+      startTime: '11:00',
+      endTime: '12:00',
+      description: '격일 일정',
+      location: '회의실',
+      category: '업무 회의',
+      repeat: { type: 'daily', interval: 2 },
+      notificationTime: 5,
+    },
+    {
+      id: '3',
+      title: '격일 일정',
+      date: '2025-02-12',
+      startTime: '11:00',
+      endTime: '12:00',
+      description: '격일 일정',
+      location: '회의실',
+      category: '업무 회의',
+      repeat: { type: 'daily', interval: 2 },
+      notificationTime: 5,
+    },
+    {
+      id: '4',
+      title: '격일 일정',
+      date: '2025-02-14',
+      startTime: '11:00',
+      endTime: '12:00',
+      description: '격일 일정',
+      location: '회의실',
+      category: '업무 회의',
+      repeat: { type: 'daily', interval: 2 },
+      notificationTime: 5,
     },
   ];
 
